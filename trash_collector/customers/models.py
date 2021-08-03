75f0aa96f81582aba6478b2a3692b064e7b70171
Amaechi McMillan <amaechimcmillan@Alexiss-iMac.attlocal.net>
2021-08-03 08:54:45 -0700

    added models to customer class

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import DateField, IntegerField
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
@@ -7,3 +8,10 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    address = models.CharField
+    zipcode = models.CharField
+    weekly_pickup_day = models.CharField
+    one_time_pickup = models.DateField(null=True)
+    balance = models.IntegerField(default=0)
+    suspend_start = models.DateField(null=True)
+    suspend_end = models.DateField(null=True)
