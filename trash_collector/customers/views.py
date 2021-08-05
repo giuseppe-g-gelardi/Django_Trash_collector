5c85ea95cc8e08bb2018353825b7d48db2088e8f
TBuscemi <thomasbuscemi124@gmail.com>
2021-08-05 15:24:32 -0400

    built the the account info display and edit in views

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -1,8 +1,8 @@
 from django.db import reset_queries
 from django.db.models.fields.related import ForeignKey
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
-from .models import Customer
+from .models import BudgetInfo, Customer
 from django.urls import reverse
 # Create your views here.
 
@@ -52,3 +52,26 @@ def one_time_pick_up(request,user ):
         'current_customer': current_customer
     }
     return render(request, "customers/index.html", context)
+
+
+def account_info(request, user):
+    if request.method == 'POST':
+        current_customer_account_info = Customer.objects.get(user_id=user.id)
+        current_customer_account_info.name = request.POST.get('name')
+        current_customer_account_info.user = request.POST.get('user')
+        current_customer_account_info.address = request.POST.get('address')
+        current_customer_account_info.zipcode = request.POST.get('zipcode')
+        current_customer_account_info.weekly_pickup_day = request.POST.get('weekly_pickup_day') 
+        current_customer_account_info.suspend_start = request.POST.get('suspend_start') 
+        current_customer_account_info.suspend_end = request.POST.get('suspend_end') 
+        current_customer_budget_info = BudgetInfo.objects.get(user_id=user.id)
+        current_customer_budget_info.user_budget = request.POST.get('user_budget')
+        current_customer_budget_info.expenses = request.POST.get('expenses')       
+        current_customer_account_info.save()
+        return HttpResponseRedirect(reverse('customers:index'))    
+    
+    current_customer_account_info = Customer.objects.get(user_id=user.id)
+    context = {
+        'current_customer_account_info': current_customer_account_info
+    }
+    return render(request, 'customers/index.html', context)
